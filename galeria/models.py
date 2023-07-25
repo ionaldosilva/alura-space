b33c8556f9d3238f6f73455a98c0812da07ae330
Ionaldo Silva <ionaldo_araujo@hotmail.com>
2023-07-25 01:55:14 -0300

    adequação do admin Django

--- galeria/models.py
+++ galeria/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from datetime import datetime
 
-# Create your models here.
+class Fotografias(models.Model):
+
+    OPCAO_CATEGORIA = [
+        ("NEBULOSA", "Nebulosa"),
+        ("GALÁXIA", "Galáxia"),
+        ("ESTRELA", "Estrela"),
+        ("PLANETA", "Planeta")
+    ]
+
+    nome = models.CharField(max_length=100, null=False, blank=False)
+    legenda = models.CharField(max_length=150, null=False, blank=False)
+    categoria = models.CharField(max_length=100, choices=OPCAO_CATEGORIA, default='')
+    decricao = models.TextField(null=False,blank=False)
+    foto = models.ImageField(upload_to="fotos/%Y/%m/%d/", blank=True)
+    publicada = models.BooleanField(default=False)
+    data = models.DateTimeField(default=datetime.now, blank=False)
+
+    def __str__(self):
+        return self.nome
--- galeria/views.py
+++ galeria/views.py
@@ -1,10 +1,13 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404
+from galeria.models import Fotografias
 
 
 def index(request):
-    return render(request, 'galeria/index.html')
 
-def imagem(request):
-    return render(request, 'galeria/imagem.html')
+    fotografia = Fotografias.objects.order_by("data").filter(publicada=True)
+    return render(request, 'galeria/index.html', {'cards':fotografia} )
+
+def imagem(request, foto_id):
+    fotografia = get_object_or_404(Fotografias, pk=foto_id)
+    return render(request, 'galeria/imagem.html', {'fotografia': fotografia})
 
